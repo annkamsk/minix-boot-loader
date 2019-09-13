0b7f31f142d18077508d329c5c604ea5778b3cb4
annkamsk <an.kramarska@gmail.com>
2019-09-13 17:52:17 +0200

    Tabs to spaces

--- mbr.s
+++ mbr.s
@@ -1,49 +1,49 @@
-org 0600h						    ; move down to 0600h
+org 0600h                           ; move down to 0600h
 
 _start:
-    cli						        ; disable interrupts
-    xor ax, ax      			    ; zero other segments
+    cli                             ; disable interrupts
+    xor ax, ax                      ; zero other segments
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 7C00h 				    ; place stack before the bootloader
-    sti								; set interrupt flag
-    cld								; zero the direction flag
-    mov si, sp						; starting address
-    mov di, 0600h					; destination address
+    mov sp, 7C00h                   ; place stack before the bootloader
+    sti                             ; set interrupt flag
+    cld                             ; zero the direction flag
+    mov si, sp                      ; starting address
+    mov di, 0600h                   ; destination address
     mov cx, 512/2
     rep movsw
 
-    jmp 0:main						; jump to the copy at 0600h, so we can load the boot sector at 7C00h
+    jmp 0:main                      ; jump to the copy at 0600h, so we can load the boot sector at 7C00h
 
 main:
     mov [DriveNo], dl               ; drive number stored in DL
 
-    mov edi, BUF					; store pointer to BUF
+    mov edi, BUF                    ; store pointer to BUF
     call print
 
 input_loop:                         ; read user input
-    mov ah, 01h						; check for key
-    int 16h							; keyboard interrupt call
-    jz	input_loop				    ; if no pending key, go back
-    mov ah, 00h						; get key
+    mov ah, 01h                     ; check for key
+    int 16h                         ; keyboard interrupt call
+    jz  input_loop                  ; if no pending key, go back
+    mov ah, 00h                     ; get key
     int 16h
-    cmp al, 121						; check if 'y'
-    je 	save_yes
-    cmp al, 110						; check if 'n'
-    je	save_no
+    cmp al, 121                     ; check if 'y'
+    je  save_yes
+    cmp al, 110                     ; check if 'n'
+    je  save_no
     jmp input_loop
 
 save_yes:
     call print_char
     mov [ans], byte 01h
-    jmp	load_bl
+    jmp load_bl
 
 save_no:
     call print_char
     mov [ans], byte 02h
 
-load_bl:						    ; load the normal boot loader
+load_bl:                            ; load the normal boot loader
                                     ; look for one active partition
     mov si, PartitionTable
     xor ax, ax
@@ -57,14 +57,14 @@ checkpartloop:
     add si, byte 16
     loop checkpartloop
 
-    cmp ax, byte 1				    ; check if only one is active
+    cmp ax, byte 1                  ; check if only one is active
     jnz bad_disk
 
-    ; save chosen by user option in the 2nd sector
+    ; save an option chosen by the user in the 2nd sector
     push di
     mov si, dapw
-    mov bx, [di+8]				    ; copy the block address
-    add bx, byte 1				    ; sector = 2
+    mov bx, [di+8]                  ; copy the block address
+    add bx, byte 1                  ; sector = 2
     mov [si+8], bx
     mov bx, [di+10]
     mov [si+10], bx
@@ -77,19 +77,19 @@ checkpartloop:
     ; load the boot sector
     push di
     mov si, dapa
-    mov bx, [di+8]				    ; copy the block address
+    mov bx, [di+8]                  ; copy the block address
     mov [si+8], bx
     mov bx, [di+10]
     mov [si+10], bx
     mov dl, [DriveNo]
     mov ah, 42h
     int 13h
-    pop si							; get partition table entry
+    pop si                          ; get partition table entry
     cmp word [7C00h+510], 0AA55h
     jne missing_os
     cli
 
-    jmp 0:7C00h						; jump to boot sector
+    jmp 0:7C00h                     ; jump to boot sector
 
 missing_os:
     xor edi, edi
@@ -101,18 +101,18 @@ bad_disk:
     mov edi, bad_disk_msg
     jmp print
 
-print:								; in edi register, function expects to get address of a buffer
+print:                              ; in edi register, function expects to get address of a buffer
 print_loop:
-    mov al, BYTE [edi]		        ; take next char from the buffer
-    inc edi							; ++idx
-    cmp al, 0x0						; if it's end of string
-    je	end_loop
-    call print_char				    ; else print char
+    mov al, BYTE [edi]              ; take next char from the buffer
+    inc edi                         ; ++idx
+    cmp al, 0x0                     ; if it's end of string
+    je  end_loop
+    call print_char                 ; else print char
     jmp print_loop
 end_loop:
     ret
 
-print_char:							; in al register, function expects to get char to print
+print_char:                         ; in al register, function expects to get char to print
     mov ah, 0xe
     int 0x10
     ret
@@ -120,28 +120,28 @@ print_char:							; in al register, function expects to get char to print
     align 4, db 0                   ; begin data area
 
 ; Error messages
-missing_os_msg  db 'Missing operating system', 13, 10, 0
-bad_disk_msg    db 'Operating system loading error', 13, 10, 0
+missing_os_msg          db 'Missing operating system', 13, 10, 0
+bad_disk_msg            db 'Operating system loading error', 13, 10, 0
 
-BUF 						db 'Enter your choice: ', 0xd, 0xa, 0x0
+BUF                     db 'Enter your choice: ', 0xd, 0xa, 0x0
 
 ; dap table for reading bootloader code
-dapa: 					dw 16							; disk address packet size
-.count:					dw 1							; block count
-.off					dw 7C00h					    ; offset of buffer
-.seg 					dw 0						    ; segment of buffer
-.lba					dd 0						    ; LBA
+dapa:                   dw 16                           ; disk address packet size
+.count:                 dw 1                            ; block count
+.off                    dw 7C00h                        ; offset of buffer
+.seg                    dw 0                            ; segment of buffer
+.lba                    dd 0                            ; LBA
                         dd 0
 
 ; dap table for saving aaaaa option
-dapw					dw 16						    ; disk address packet size
-.countw					dw 1							; block count
-.offw					dw 0600h					    ; offset of buffer
-.segw					dw 0						    ; segment of buffer
-.lbaw 					dd 0							; LBA
+dapw                    dw 16                           ; disk address packet size
+.countw                 dw 1                            ; block count
+.offw                   dw 0600h                        ; offset of buffer
+.segw                   dw 0                            ; segment of buffer
+.lbaw                   dd 0                            ; LBA
                         dd 0
 
-ans						db 0                            ; saved option: 1 for yes, 2 for no
+ans                     db 0                            ; saved option: 1 for yes, 2 for no
 
 PartitionTable          equ $$+446                      ; start of partition table
 DriveNo                 equ 0800h
